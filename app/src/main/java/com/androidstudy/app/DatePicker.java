332ac27b367304dfa481925d443cda0ab7b8a95a
santa-mk <maguielaw.elir.107@gmail.com>
2014-05-18 15:54:04 +0900

    add datePickerDialog

--- app/src/main/java/com/androidstudy/app/DatePicker.java
+++ app/src/main/java/com/androidstudy/app/DatePicker.java
@@ -1,19 +1,49 @@
 package com.androidstudy.app;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 
 public class DatePicker extends ActionBarActivity {
+    private Activity mActivity;
+    DatePickerDialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mActivity = this;
         setContentView(R.layout.activity_date_picker);
+        showDialog();
     }
 
+
+    private void showDialog() {
+        DatePickerDialog.OnDateSetListener mListener = new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(android.widget.DatePicker datePicker, int year, int month, int day) {
+                String date = year + "/" + month + "/" + day;
+                Toast.makeText(mActivity, date, Toast.LENGTH_LONG).show();
+                TextView view = (TextView) findViewById(R.id.text_input);
+                view.setText(date);
+            }
+        };
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog dialog = new DatePickerDialog(this, mListener, year, month, day);
+        dialog.show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
